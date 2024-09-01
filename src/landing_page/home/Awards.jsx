b0204cee44a4c9fc70b289bea467331113f65e5a
“Nikhil” <nikdc03x@gmail.com>
2024-09-01 17:59:23 +0530

    hoem component

--- src/landing_page/home/Awards.jsx
+++ src/landing_page/home/Awards.jsx
@@ -2,7 +2,37 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+<div className="container mt-5 mb-5">
+     <div className="row">
+               <div className="col-6 p-5">
+                    <img src="media/images/largestBroker.svg" alt="" />
+               </div>
+               <div className="col-6 p-5 mt-3">
+                <h1>Largest Stock Broker in India</h1>
+            <p>2+ million TradingPal clients contribute to over 15% of all retail order volumes in india daily by tracking and investing in </p>
+            <div className="row">
+                <div className="col-6 ">
+                  <ul>
+                    <li><p> Futures and Options</p></li>
+                    <li> <p> Commodity Dervatives</p> </li>
+                    <li> <p> Currency Derivatives</p> </li>
+                  </ul>
+                </div>
+                <div className="col-6">
+                  <ul>
+                    <li> <p> Stocks & IPOs</p></li>
+                    <li> <p> Direct Mutual Funds</p></li>
+                    <li> <p> Bonds and Govt. Securities</p></li>
+                 </ul>
+                </div>
+                    </div>
+                    <img src="media\images\pressLogos.png" alt="" style={{width:"90%"}}/>
+        </div>
+    </div>
+ </div>
+            
+        
+            
     );
 }
 
