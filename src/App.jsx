5fe64ffe610e1b7ad947ba12d93e6c5c0f97dd48
“Nikhil” <nikdc03x@gmail.com>
2024-09-04 23:39:33 +0530

    init react-router in vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-
+import { Route, Routes } from "react-router-dom"
+import HomePage from './landing_page/home/HomePage.jsx'
+import AboutPage from './landing_page/about/AboutPage.jsx'
+import ProductPage from './landing_page/products/ProductPage.jsx'
+import Supportpage from './landing_page/support/SupportPage.jsx'
+import Signup from './landing_page/signup/Signup.jsx'
+import PricingPage from './landing_page/pricing/PricingPage.jsx'
+import Navbar from './Navbar.jsx'
+import Footer from './Footer.jsx'
 
 
 function App() {
@@ -6,7 +14,18 @@ function App() {
 
   return (
     <>
-      
+<div className="App">
+      <Navbar/>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/> 
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/about' element={<AboutPage/>}/>
+      <Route path='/product' element={<ProductPage/>}/>
+      <Route path='/pricing' element={<PricingPage/>}/>
+      <Route path='/support' element={<Supportpage/>}/>
+  </Routes>
+    <Footer/>
+</div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import{BrowserRouter,Routes,Route} from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import HomePage from './landing_page/home/HomePage.jsx'
-import AboutPage from './landing_page/about/AboutPage.jsx'
-import ProductPage from './landing_page/products/ProductPage.jsx'
-import Supportpage from './landing_page/support/SupportPage.jsx'
-import Signup from './landing_page/signup/Signup.jsx'
-import PricingPage from './landing_page/pricing/PricingPage.jsx'
-import Navbar from './Navbar.jsx'
-import Footer from './Footer.jsx'
-// import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/> 
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/about' element={<AboutPage/>}/>
-      <Route path='/product' element={<ProductPage/>}/>
-      <Route path='/pricing' element={<PricingPage/>}/>
-      <Route path='/support' element={<Supportpage/>}/>
-  </Routes>
-    <Footer/>
-  </BrowserRouter>,
-);
+import './index.css'
+import { BrowserRouter } from "react-router-dom";
+ 
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
